276a49e6d4fc10ad403630b61c0ac57b9d734a2c
dwanford <dwanford@hotmail.com>
2022-11-10 21:56:40 +0200

    adding category controller

---
+++ src/test/java/com/dwanford/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,49 @@
+package com.dwanford.spring5webfluxrest.controllers;
+
+import com.dwanford.spring5webfluxrest.domain.Category;
+import com.dwanford.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @BeforeEach
+    void setUp() {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    void list() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("Foo").build(),
+                                    Category.builder().description("Bar").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void getById() {
+        BDDMockito.given(categoryRepository.findById("someId"))
+                .willReturn(Mono.just(Category.builder().description("Foo").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/someId")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
